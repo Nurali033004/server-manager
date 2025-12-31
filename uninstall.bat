15ddfb0c74432065960bb665bc8f7ae5106abfe7
Nurali033004 <worldsolo0330@gmail.com>
2025-12-31 16:36:05 +0500

    Fix uninstall.bat to avoid file locking

--- uninstall.bat
+++ uninstall.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell -ExecutionPolicy Bypass -File "%~dp0uninstall.ps1"
+powershell -Command "iwr -Uri 'https://raw.githubusercontent.com/Nurali033004/server-manager/main/uninstall.ps1' -OutFile $env:TEMP\uninstall.ps1; & $env:TEMP\uninstall.ps1"
